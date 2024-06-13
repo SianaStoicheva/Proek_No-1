0e32a5604810cb52451fc25992abd808c8ac5176
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-06-13 19:41:50 +0300

    proekt

---
+++ BroeneNaNuliVMasiv/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BroeneNaNuliVMasiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            int zeroCount = 0;
+
+            Console.WriteLine("Vuvedete 10 chisla");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Chislo {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int number in numbers)
+            {
+                if (number == 0)
+                {
+                    zeroCount++;
+                }
+            }
+
+            Console.WriteLine($"Broqt na nulite e: {zeroCount}");
+        }
+    }
+}
---
+++ ZadachaSVuvedenMasiv/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ZadachaSVuvedenMasiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete 20 elementa:");
+            double[] array = new double[20];
+            int count = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write($"Chislo {i + 1}: ");
+                double input = Convert.ToDouble(Console.ReadLine());
+
+                if (input == 0)
+                {
+                    break;
+                }
+                array[i] = input;
+                count++;
+            }
+
+            int emptySlots = 20 - count;
+
+            if (count > 0)
+            {
+                double minValue = array[0];
+                double maxValue = array[0];
+                int minIndex = 0;
+                int maxIndex = 0;
+
+                for (int i = 1; i < count; i++)
+                {
+                    if (array[i] < minValue)
+                    {
+                        minValue = array[i];
+                        minIndex = i;
+                    }
+                    if (array[i] > maxValue)
+                    {
+                        maxValue = array[i];
+                        maxIndex = i;
+                    }
+                }
+
+                Console.WriteLine($"{emptySlots}");
+                Console.WriteLine($"Nai-malko chislo: {minValue} poziciq {minIndex + 1}");
+                Console.WriteLine($"Nai-golqmo chislo: {maxValue} poziciq {maxIndex + 1}");
+            }
+            else
+            {
+                Console.WriteLine("Nqmahe vuvedeni chisla.");
+            }
+        }
+    }
+}
